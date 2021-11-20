9839a9822da911d8df5d23ea6939261bbbe6abbe
RubiMiranda <91550567+RubiMiranda@users.noreply.github.com>
2021-11-19 20:02:15 -0600

    Add files via upload

---
+++ Fun3.m
@@ -0,0 +1,16 @@
+"Octave Script"
+% Title			    :Función plinomial y racionales .
+% Description		:Script para funciones  polinimial y racional
+% Author		    :Miranda Cruz Roxana Rubi
+% Date		    	:20211114
+% Version		    :1
+% Usage			    :octave>cd /path/
+% 		        	:octave>Funciones para polinomial y racional
+% 		        	:Requiere aplicación octave, usar su línea de comandos 
+clear
+"FUNCION POLINOMIAL"
+"FUNCION"
+"Rango"
+fx=sqrt (x.^2)
+x=-10:0.1:10;
+plot(fx,x)
